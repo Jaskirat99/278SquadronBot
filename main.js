67ebe8ca7790f5dbe21cacd9dd5c1ca0a82d1885
Jaskirat99 <60302907+Jaskirat99@users.noreply.github.com>
2021-06-20 19:54:46 -0700

    added servers, clearchannel and status commands

--- main.js
+++ main.js
@@ -6,11 +6,35 @@ const command = require('./command')
 
 client.on('ready', () => {
     console.log('Client ready!');
-    client.user.setActivity('Official Squadron Bot!');
 
     command(client, ['ping', 'test'], (message) => {
         message.channel.send('Pong!')
     })
+
+    command(client, 'servers', (message) =>{
+        client.guilds.cache.forEach((guild) => {
+            message.channel.send(
+                `${guild.name} has a total of ${guild.memberCount} members! `)
+        })
+    })
+     command(client, ['cc', 'clearchannel'], message =>{
+         if(message.member.hasPermission('ADMINSTRATOR')) {
+             message.channel.messages.fetch().then((results) =>{
+                 message.channel.bulkDelete(results)
+             })
+         }
+     })
+
+     command(client, 'status', message =>{
+         const content = message.content.replace('.status ', '')
+
+         client.user.setPresence({
+             activity: {
+                 name: content,
+                 type: 0,
+             },
+         })
+     })
     });
 
 
