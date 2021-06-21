a0d80515aa224df5b55dd9cebac7068aa4d1853f
Jaskirat99 <60302907+Jaskirat99@users.noreply.github.com>
2021-06-21 13:07:12 -0700

    added help menu

--- main.js
+++ main.js
@@ -29,17 +29,22 @@ client.on('ready', () => {
     command(client, 'status', message =>{
          const content = message.content.replace('.status ', '')
 
+         if(message.member.hasPermission('ADMINISTRATOR')) {
          client.user.setPresence({
              activity: {
                  name: content,
                  type: 0,
-             },
-         })
-     })     
+             }
+            })
+        }
+    })
+        
+     
     
     command(client, 'createtextchannel', (message) => {
         const name = message.content.replace('.createtextchannel ', '')
     
+        if(message.member.hasPermission('ADMINISTRATOR')) {
         message.guild.channels
           .create(name, {
             type: 'text',
@@ -48,11 +53,13 @@ client.on('ready', () => {
             const categoryId = '855211234889564191'
             channel.setParent(categoryId)
           })
+        }
       })
     
     command(client, 'createvoicechannel', (message) => {
         const name = message.content.replace('.createvoicechannel ', '')
     
+        if(message.member.hasPermission('ADMINISTRATOR')) {
         message.guild.channels
           .create(name, {
             type: 'voice',
@@ -62,39 +69,10 @@ client.on('ready', () => {
             channel.setParent(categoryId)
             channel.setUserLimit(10)
           })
+        }
       })
 
-    command(client, 'embed', (message) => {
-   
-      const embed = new Discord.MessageEmbed()
-      .setTitle('Example text embed')
-      .setURL('https://www.surreycadets.ca/')
-      .setFooter('This is a footer')
-      .setColor('#FFD700')
-      .addFields(
-        {
-          name: 'Field 1',
-          value: 'Hello world',
-          inline: false,
-        },
-        {
-          name: 'Field 2',
-          value: 'Hello world',
-          inline: false,
-        },
-        {
-          name: 'Field 3',
-          value: 'Hello world',
-          inline: false,
-        },
-        {
-          name: 'Field 4',
-          value: 'Hello world',
-        }
-      )
-
-    message.channel.send(embed)
-    });
+    
 
     command(client, 'serverinfo', (message) => {
         const { guild } = message
@@ -108,26 +86,43 @@ client.on('ready', () => {
           .setURL('https://www.surreycadets.ca/')
           .setColor('#FFD700')
           .addFields(
-            {
-              name: 'Region',
-              value: region,
-            },
-            {
-              name: 'Members',
-              value: memberCount,
-            },
-            {
-              name: 'Owner',
-              value: owner.user.tag,
-            },
-            {
-              name: 'AFK Timeout',
-              value: afkTimeout / 60,
-            }
+            {name: 'Region', value: region,},
+            {name: 'Members',value: memberCount,},
+            {name: 'Owner', value: owner.user.tag,},
+            {name: 'AFK Timeout', value: afkTimeout / 60,}
           )
-    
         message.channel.send(embed)
       })
+
+      command(client, 'help', (message) => {
+        const { guild } = message
+        const icon = guild.iconURL()  
+        const embed = new Discord.MessageEmbed()
+
+        .setTitle(`**Welcome To The Squadron Discord Bot\'s Help Menu!**`)
+        .setThumbnail(icon)
+        .setURL('https://www.surreycadets.ca/')
+        .setColor('#FFD700')
+        .setTimestamp()
+        .setFooter('278 Cormorant Squadron')
+        .addField("Prefix",'The Current Prefix For the Bot Is \'.\' ',)
+        .addField('\u200B', '\u200B')
+        .addField("Createtextchannel",'creates a text channel with the specified name (ADMIN ONLY)', )
+        .addField('\u200B', '\u200B')
+        .addField("Createvoicechannel",'creates a voice channel with the specified name (ADMIN ONLY)', )
+        .addField('\u200B', '\u200B')
+        .addField("Serverinfo",'Displays stats of the current server', )
+        .addField('\u200B', '\u200B')
+        .addField("Cc / clearchannel",'Clears all messages in a channel (ADMIN ONLY)', )
+        .addField('\u200B', '\u200B')
+        .addField("Status",'Clears all messages in a channel (ADMIN ONLY)', )
+        .addField('\u200B', '\u200B')
+        .addField("Servers",'Changes the Bot\'s status (ADMIN ONLY)', )
+        .addField('\u200B', '\u200B')
+        .addField("Help",'Displays this menu ', )           
+    
+        message.channel.send(embed)
+    })
 })
     
 
