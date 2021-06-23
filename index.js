3170594f22e64176ab9ff15f224bfa07f40c9632
Jaskirat99 <60302907+Jaskirat99@users.noreply.github.com>
2021-06-23 13:48:15 -0700

    add fast type and bot info

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 require('module-alias/register')
 
-const Discord = require('discord.js')
-const client = new Discord.Client()
+//const Discord = require('discord.js')
+//const client = new Discord.Client()
 
-
+const path = require('path')
+const Commando = require('discord.js-commando')
 
 const config = require('@root/config.json')
 const welcome = require('@features/welcome')
@@ -11,11 +12,26 @@ const loadCommands = require('@root/commands/load-commands')
 const commandBase = require('@root/commands/command-Base')
 
 
+const client = new Commando.CommandoClient({
+  owner: '763867677256712205',
+  commandPrefix: config.prefix,
+})
 
 client.on('ready', async () => {
   console.log('The client is ready!')
 
-  welcome(client, Discord)
+  client.registry
+    .registerGroups([
+      ['misc', 'misc commands'],
+      ['moderation', 'moderation commands'],
+      ['economy', 'Commands for the economy system'],
+      ['giveaway', 'Commands to manage giveaways'],
+      ['games', 'Commands to handle games'],
+    ])
+    .registerDefaults()
+    .registerCommandsIn(path.join(__dirname, 'cmds'))
+
+  welcome(client)
   loadCommands(client)
 
 })
